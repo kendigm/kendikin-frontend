7fbca7d12da280dd74089a596000df6793df7e3e
kendigm <Kendiigm@gmail.com>
2024-05-06 22:55:55 +0500

    Assets Images logo

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,12 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
+
+const App = () => {
 
   return (
     <>
-      <div>
 
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Kendikin</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
+      <h1 className="text-3xl font-bold underline">Kendikin</h1>
 
-      </div>
-      <p className="read-the-docs">
-        Click on the Kendikin logos to learn more
-      </p>
     </>
   )
 }
